cfbc0ba6eca8699d6a023dde9f0f258151fd5bb5
rence <99105241+rencejke@users.noreply.github.com>
2024-03-22 15:04:02 +0800

    updated complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -30,8 +30,8 @@ burgir.addEventListener("click", () =>{
     burgir.classList.toggle("open");
     menu.classList.toggle("open");
 });
-
-const links = document.querySelectorAll(".faqs__item h4");
+  
+const links = document.querySelectorAll(".faqs__questions");  //.faqs__item h4
 links.forEach((link) => {   
 link.addEventListener("click", () => {
 link.querySelector("i").classList.toggle("open");
